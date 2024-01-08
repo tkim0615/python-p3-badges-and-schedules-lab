fa3b421fc1be3a51f9ee7168c308bf0fd06504b5
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-08 18:25:37 -0500

    completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,36 @@
+import ipdb
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    # badge_batch = list()                                      #for loop
+    # for i in names:
+    #     badge_batch.append(f'Hello, my name is {i}.')
+    # return badge_batch
+    badge_batch =[f'Hello, my name is {i}.' for i in names]      # list constructor
+    return badge_batch
+
+
+# index = animals.index('dog')
 
 def assign_rooms(names):
-    return None
+    # room_list = []  #for loop
+    # for name in names:
+    #     index= names.index(name)
+    #     room_list.append(f"Hello, {name}! You'll be assigned to room {index + 1}!")
+    # return room_list
+    room_list = [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]  #list constructor
+    return room_list
+
 
 def printer(names):
-    return None
+    list=batch_badge_creator(names)
+    for name in list:
+        print(name)
+
+    list2=assign_rooms(names)
+    for name in list2:
+        print(name)
+
+printer(['ty', 'tptptp'])
+
